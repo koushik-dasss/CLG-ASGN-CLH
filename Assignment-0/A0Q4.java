aedc74032c29578dc26c39e45d463c06a34c9b42
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-10-29 22:39:17 +0530

    Added A0Q4 , importance of \t and \n

---
+++ Assignment-0/A0Q4.java
@@ -0,0 +1,10 @@
+public class A0Q4 {
+    public static void main(String[] args){
+        System.out.println("*\n*\t*\n*\t*\t*\n*\t*\t*\t*");
+        System.out.println("*\t*\t*\t*\t*");
+        
+        //Pattern problem to understand the importance of \n and \t
+    }
+
+    
+}
