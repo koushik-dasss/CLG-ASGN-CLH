ab1041145dbdbbdcec23c5074211bea89acdf5f5
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-10-29 22:10:40 +0530

    Added A0Q2

---
+++ Assignment-0/A0Q2.java
@@ -0,0 +1,14 @@
+public class A0Q2 {
+    public static void main(String[] args){
+        
+        //Two Lines of Code to print two different lines
+        System.out.println("My name is Koushik Das");
+        System.out.println("My branch is CSIT");
+
+        //OR
+        //Single line of code to print two different lines or multiple lines 
+        System.out.println("My name is Koushik Das"+"\nMy branch is CSIT");
+    
+    }
+    
+}
