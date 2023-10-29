4df901b8e663817b8051758613b1bb923e614c70
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-10-29 22:52:19 +0530

    Added A0Q5 , " for double qoutes inside the console and \' for single quotes inside the console

---
+++ Assignment-0/A0Q5.java
@@ -0,0 +1,11 @@
+public class A0Q5 {
+    public static void main(String[] args){
+        System.out.println("Welcome \"Coder\"");
+
+
+        // prints the message "Welcome "Coder"" to the console. 
+        //The \" is used to include double quotes within the string.
+        // \' is used to include single quotes within the string.
+    }
+    
+}
