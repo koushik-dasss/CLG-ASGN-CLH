4853c14499cf51accedeff88ae28b6384768bb27
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-10-29 22:26:45 +0530

    Added A0Q3 \t is used to give tab space , it creates a space but gives the output in the same line

---
+++ Assignment-0/A0Q3.java
@@ -0,0 +1,16 @@
+public class A0Q3 {
+    public static void main(String[] args){
+                System.out.println("My name is Koushik Das");
+                System.out.println("My name is Koushik\t Das");
+                System.out.println("My name is Koushik Das\tMy branch is CSIT");
+        
+                // \t gives a tab space , but remains in the same line while printing as output in the console 
+        
+                // Each \t represents a tab character, and the console or output device will align the text to the next tab stop. 
+        
+                //This is commonly used to format data in columns when printing information to the console 
+        
+                //or when constructing formatted strings.
+    }
+    
+}
