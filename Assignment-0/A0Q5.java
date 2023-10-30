e9c0efbf6ab46604b3e79a1bf77917c7acde780b
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-10-30 21:16:34 +0530

    A0Q5 single quote added \' \'

--- Assignment-0/A0Q5.java
+++ Assignment-0/A0Q5.java
@@ -1,9 +1,11 @@
 public class A0Q5 {
     public static void main(String[] args){
-        System.out.println("Welcome \"Coder\"");
+        System.out.println("Welcome \"Coder\""); //double quote insider string 
+        System.out.println("Welcome \'Coder\'"); //single quote insider string
 
 
-        // prints the message "Welcome "Coder"" to the console. 
+
+        // prints the message "Welcome "Coder" in the console. 
         //The \" is used to include double quotes within the string.
         // \' is used to include single quotes within the string.
     }
