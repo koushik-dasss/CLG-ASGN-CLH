6da9a258e1484ae465640ac976e290275c00486d
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-10-31 09:20:23 +0530

    Added A1Q5.java

---
+++ LabAssignment-1/A1Q5.java
@@ -0,0 +1,12 @@
+public class A1Q5 {
+    public static void main(String[] args){
+        String name = "Koushik";
+        String message = "Good Morning";
+        System.out.println("Hey "+ name+", "+ message+"!");
+    
+    
+    }
+    
+
+    
+}
