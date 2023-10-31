a8c2a0bc682bd44164c18f0df00065bfb50e166a
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-10-31 22:11:49 +0530

    A1Q6 added, data types importance

---
+++ LabAssignment-1/A1Q6.java
@@ -0,0 +1,11 @@
+public class A1Q6 {
+    public static void main(String[] args){
+        int Account_No = 123456;
+        String name = "Koushik";
+        double balance = 7654.98;
+        System.out.println("My name is "+name+" bearing  account number "+ Account_No+" having  balance  "+ balance);
+
+
+    }
+    
+}
