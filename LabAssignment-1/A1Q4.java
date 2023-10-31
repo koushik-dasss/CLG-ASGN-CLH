00f7ad0ad2b7a8ab6be461a1a78630f67c8841ff
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-10-31 09:12:37 +0530

    A1Q4 added, new line character for showing in two lines from  one single line of code

---
+++ LabAssignment-1/A1Q4.java
@@ -0,0 +1,8 @@
+public class A1Q4 {
+    public static void main(String[] args){
+        System.out.println("Koushik\nDas");
+        // New line escaping sequence \n  character takes the cursor to the next statement in the console 
+        // being in the same line of code 
+    }
+    
+}
