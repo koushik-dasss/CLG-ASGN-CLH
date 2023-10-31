a4fa647804964b953b1dc056cbbc36a9003be0f2
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-10-31 22:46:02 +0530

    Added A1Q7 , A1Q8

--- LabAssignment-1/A1Q7.java
+++ LabAssignment-1/A1Q7.java
@@ -1,11 +1,12 @@
 public class A1Q7 {
     public static void main(String[] args){
-        int x =10;
-        int y=20;
-        int z =x+y; //30
-        x=z-x; //20
-        y=z-x; //10
-        System.out.println("Swapped value of x is : "+ x+"\nSwapped value of y is : "+y);
+        int a=10;
+        int b=20;
+        int c=a;
+        a=b;
+        b=c;
+        System.out.println(a);
+        System.out.println(b);
+
     }
-    
 }
---
+++ LabAssignment-1/A1Q8.java
@@ -0,0 +1,20 @@
+public class A1Q8 {
+    public static void main(String[] args){
+        int x=10;
+        int y=20;
+        x=x+y; //30
+        y=x-y; //10
+        x=x-y; //20
+        
+        System.out.println("Swapped value of x is : "+ x+"\nSwapped value of y is : "+y);
+    }
+    
+}
+
+        
+
+        
+
+    
+    
+
