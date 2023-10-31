08b9e5d05763fa15e925f427cfec19ae4b86606e
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-10-31 09:30:25 +0530

    A1Q7.java added + swapped two variables using a third variable

---
+++ LabAssignment-1/A1Q7.java
@@ -0,0 +1,11 @@
+public class A1Q7 {
+    public static void main(String[] args){
+        int x =10;
+        int y=20;
+        int z =x+y; //30
+        x=z-x; //20
+        y=z-x; //10
+        System.out.println("Swapped value of x is : "+ x+"\nSwapped value of y is : "+y);
+    }
+    
+}
