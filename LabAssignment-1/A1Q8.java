d480be2d605c220478d1fcc3d4e0a5ffe15fd05b
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-01 08:58:29 +0530

    Swapped values of two variables without using value of third variable

--- LabAssignment-1/A1Q8.java
+++ LabAssignment-1/A1Q8.java
@@ -2,9 +2,9 @@ public class A1Q8 {
     public static void main(String[] args){
         int x=10;
         int y=20;
-        x=x+y; //30
-        y=x-y; //10
-        x=x-y; //20
+        x=x+y; //10+20=30
+        y=x-y; //30-20=10
+        x=x-y; //30-10=20
         
         System.out.println("Swapped value of x is : "+ x+"\nSwapped value of y is : "+y);
     }
