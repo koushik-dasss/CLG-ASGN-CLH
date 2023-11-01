8705d9bcb50711b724ef5ea2d34618201d801c6a
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-01 08:54:17 +0530

    Swapped values of two variables using third variable

--- LabAssignment-1/A1Q7.java
+++ LabAssignment-1/A1Q7.java
@@ -2,11 +2,11 @@ public class A1Q7 {
     public static void main(String[] args){
         int a=10;
         int b=20;
-        int c=a;
-        a=b;
-        b=c;
+        int c=a; //c=10
+        a=b; //a=20
+        b=c; //b=10
         System.out.println("Swapped value of a is:"+a+"\nSwapped value of b is:"+b);
-        
+
 
     }
 }
