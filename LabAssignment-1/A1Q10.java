e4fd4725da5f69a9a62a4946a212e2bf2d5ea2ec
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-01 09:33:26 +0530

    Optimised code of pattern

---
+++ LabAssignment-1/A1Q10.java
@@ -0,0 +1,15 @@
+public class A1Q10 {
+    public static void main(String[] args){
+        String S1="1";
+        System.out.println(S1);
+        System.out.println(S1+2+S1);
+        S1=S1+2+S1;
+        System.out.println(S1+3+S1);
+        S1=S1+3+S1;
+        System.out.println(S1+4+S1);
+
+
+
+    }
+    
+}
