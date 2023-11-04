2c6d194722d05bfeb7f2029f47b4c7088cb00221
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-04 09:28:25 +0530

    Added classwork codes + partial temporary changes

---
+++ ClassworkCodes/DataTypeExamples.java
@@ -0,0 +1,63 @@
+package ClassworkCodes;
+
+public class DataTypeExamples {
+    public static void main(String[] args){
+        byte v1 = 35;
+        System.out.println("The byte value is :"+ v1);
+        int v2 = 2000;
+        System.out.println("The int value is :"+ v2);
+        short v3 = 20000;
+        System.out.println("The short value is :"+ v3);
+        long v4 = 345678; /*  L/l suffix is not to be used compulsorily while declaring a variable of a long type
+         according to  modern compiler */  
+        System.out.println("The long value is :"+ v4);
+        long v5 = 345678l;
+        System.out.println("The long value is :"+ v5);
+        long v6 = 345678L;
+        System.out.println("The long value is :"+ v6);
+        float v7 = 2.111f; // F/f  suffix is compulsory
+        System.out.println("The float value is :"+ v7);
+        float v8 = 2.111F; // F/f suffix is  compulsory
+        System.out.println("The float value is :"+ v8);
+        double v9 = 2.11111111d; // D/d suffix is not  compulsory
+        System.out.println("The double  value is :"+ v9);
+        double v10 = 2.1111111111111D; // D/d suffix is not   compulsory
+        System.out.println("The double  value is :"+ v10);
+        double v11 = 2.111111111111111111111111111111; // D/d suffix is  not  compulsory
+        System.out.println("The double  value is :"+ v11);
+        boolean v12 = true;
+        System.out.println("The boolean  value is :"+ v12);
+        boolean v13 = false;
+        System.out.println("The boolean  value is :"+ v13);
+        char ch ='A';
+        System.out.println("The character  value is :"+ ch);
+        
+
+
+
+
+
+        
+
+
+
+        
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+    }
+
+
+    
+}
--- LabAssignment-1/HA1Q5.java
+++ LabAssignment-1/HA1Q5.java
@@ -1,35 +1,32 @@
 public class HA1Q5 {
     public static void main(String[] args){
         // non-optimized code (extra one variable is declared , can be done using 4 variables's arithmetic operations )
-        int W=10, G=20,K=30,A=40, c= 0;
-        c=W;
-        W=G;
-        G=K;
-        K=A;
-        A=c;
+        /*int W=10, G=20,K=30,A=40, temp= 0; // USING EXTRA VARIABLE 
+        temp=W; // temp = 10
+        W=G; // W = 20
+        G=K; // G = 30
+        K=A; // K = 40
+        A=temp; // A = 10
+        System.out.println(W);
+        System.out.println(G);
+        System.out.println(K);
+        System.out.println(A);*/
+
+
+        // optimised code with 4 variables (arithmetic operations)
+        // WITHOUT USING EXTRA VARIABLE
+
+        int W=10, G=20,K=30,A=40;
+        // the value of W will move to A, A to K, K to G and finally G to W
+
+        A= A+K+G+W; // A = 40+30+20+10 = 100
+        W= A-(G+K+W);
+              
+
         System.out.println(W);
         System.out.println(G);
         System.out.println(K);
         System.out.println(A);
-
-
-        // optimised code with 4 variables (arithmetic operations)
-
-        W=W+G;
-        G=W-G;
-        W=W-G;
-
-        G=G+K;
-        K=G-K;
-        G=G-K;
-
-        K=A+K;
-        A=A-K;
-        K=A-K;
-
-        A=A+W;
-        W=A-W;
-        A=A-W;
 
 
         
