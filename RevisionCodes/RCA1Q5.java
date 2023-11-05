e3fd60381b43f958135969c675e0e5cc87a8aed8
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-05 11:52:00 +0530

    Name and message display using variable storage

---
+++ RevisionCodes/RCA1Q5.java
@@ -0,0 +1,10 @@
+package RevisionCodes;
+
+public class RCA1Q5 {
+    public static void main(String args[]){
+        String name="Koushik";
+        String message="Good Morning";
+        System.out.println("Hey "+name+", "+ message+ "!" );
+    }
+    
+}
