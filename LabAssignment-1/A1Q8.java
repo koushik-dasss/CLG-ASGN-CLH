0fedeef0a30dbcb1708c4354ae8c5add8484d010
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-05 19:43:36 +0530

    exchanged + optimised + revised

--- LabAssignment-1/A1Q8.java
+++ LabAssignment-1/A1Q8.java
@@ -1,12 +1,19 @@
 public class A1Q8 {
     public static void main(String[] args){
-        int x=10;
-        int y=20;
+        /*int x=10, y=20;
         x=x+y; //10+20=30
         y=x-y; //30-20=10
         x=x-y; //30-10=20
         
-        System.out.println("Swapped value of x is : "+ x+"\nSwapped value of y is : "+y);
+        System.out.println("Exchanged value of x is : "+ x+"\nExchanged value of y is : "+y);*/
+
+
+        int x=10,y=20;
+        y=x+y; //10+20=30
+        x=y-x; //30-10=20
+        y=y-x; //30-20=10
+        System.out.println("Exchanged value of x is:"+x+"\nExchanged value of y is:"+y);
+
     }
     
 }
