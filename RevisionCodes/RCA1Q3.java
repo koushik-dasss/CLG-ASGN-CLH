90829208be83a9dbd4954ea6209049de49620df1
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-05 11:02:51 +0530

    42 # pattern with gap revised

---
+++ RevisionCodes/RCA1Q3.java
@@ -0,0 +1,17 @@
+package RevisionCodes;
+
+public class RCA1Q3 {
+    public static void main(String args[]){
+        System.out.println("##########################################");
+        System.out.println("##                                      ##");
+        System.out.println("##                                      ##");
+        System.out.println("##                                      ##");
+        System.out.println("##                                      ##");
+        System.out.println("##########################################");
+             
+      
+
+
+    }
+    
+}
