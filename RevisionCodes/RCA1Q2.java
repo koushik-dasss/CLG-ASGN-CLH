35c4f24c5252dabf9a15610e98b753fc027517b7
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-05 11:54:28 +0530

    7 star 5 gap pattern question revised

--- RevisionCodes/RCA1Q2.java
+++ RevisionCodes/RCA1Q2.java
@@ -1,6 +1,6 @@
 package RevisionCodes;
 
-public class RCA2Q2 {
+public class RCA1Q2 {
     public static void main(String args[]){
         System.out.println("*******     *******");
         System.out.println("      *     *     *");
