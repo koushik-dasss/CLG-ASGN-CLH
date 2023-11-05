387bd6a6bf2626b626720dc4b80fa716224615b6
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-05 13:39:19 +0530

    Revised exchanging of values of two variables using third temp variable with optimised lines of code

--- RevisionCodes/RCA1Q7.java
+++ RevisionCodes/RCA1Q7.java
@@ -2,22 +2,20 @@ package RevisionCodes;
 
 public class RCA1Q7 {
     public static void main(String args[]){
-        /*int x =10,y=20,z=0;
-        z=y; // z = y = 20
-        y=x; // y = x = 10
-        x=z; // x = z = 20
-        System.out.println("Exchanged value of x:"+x);
-        System.out.println("Exchanged value of y:"+y);*/
-        
-        // OR (same logic)
         /*int x=10,y=20,z=0;
         z=x; // z = x = 10
         x=y; // x = y = 20
         y=z; // y = z = 10
-        System.out.println("Exchanged value of x:"+x);
-        System.out.println("Exchanged value of y:"+y);*/
-
+        System.out.println("Exchanged value of x is:"+x+"\nExchanged value of y is:"+y); */
         
+
+        // OR(same logic )
+
+        /* int x=10,y=20,z=0;
+        z=y; // z = y = 20
+        y=x; // y = x = 10
+        x=z; // x = z = 20
+        System.out.println("Exchanged value of x is:"+x+"\nExchanged value of y is:"+y); */
     }
-    
 }
+        
