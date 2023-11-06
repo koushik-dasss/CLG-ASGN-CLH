8085be29b9c3fc105181141bd98910dd6005fe7c
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-06 09:26:11 +0530

    Practical Class-1 codes added for revision

--- ClassworkCodes/PracticalClass1.java
+++ ClassworkCodes/PracticalClass1.java
@@ -3,6 +3,8 @@ package ClassworkCodes;
 public class PracticalClass1 {
     public static void main(String[] args){
         // Assignment 0-1 questions .
+        // Single line comment
+        /* multi line comment  */
 
 
 
