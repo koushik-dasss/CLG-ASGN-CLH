ae7fe3ab666fae1e1eddf6794b1559418c191c40
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-06 08:44:18 +0530

    Output questions correction made

--- LabAssignment-1/A1Q9.java
+++ LabAssignment-1/A1Q9.java
@@ -1,11 +1,12 @@
 public class A1Q9 {
     public static void main(String[] args){
-        System.out.println(5);
-        System.out.println((float)(25/6));
-        System.out.println(5+"6");
-        System.out.println(5+7+"9");
-        System.out.println("92"+7+5);
-        System.out.println(2+"9");
+        System.out.println(5); // 5 direct integer output
+        System.out.println((float)25/6); // 25/6 = 4 (int. with normal division) 25/6= 4.1666665 
+        // First 25 is converted to float , then divided by int. 6 = 25.0/6 = 4.1666665
+        System.out.println(5+'6'); // 5+ASCII value of 6 = 5 + 54 = 59
+        System.out.println(5+7+'9'); // 12+ ASCII value of 9 = 12 + 57 = 69
+        System.out.println("92"+7+5); // 92+"7"=927+"5"=9275
+        System.out.println(2+"9"); //29
 
 
 
