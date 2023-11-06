aa2e8f63797ea3d589808b73033f0e7f3f0ceee4
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-06 22:40:09 +0530

    Practical class -3 added

--- ClassworkCodes/PracticalClass3.java
+++ ClassworkCodes/PracticalClass3.java
@@ -1,5 +1,48 @@
 package ClassworkCodes;
+import java.util.Scanner;
+
 
 public class PracticalClass3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a byte :");
+        byte v1 = sc.nextByte();
+        System.out.println("The byte value is:"+v1);
+        System.out.println("Enter an int :");
+        int v2=sc.nextInt();
+        System.out.println("The int value is:"+v2);
+        System.out.println("Enter a short :");
+        short v3=sc.nextShort();
+        System.out.println("The short value is:"+v3);
+        System.out.println("Enter a long:");
+        long v4 = sc.nextLong();
+        System.out.println("The long value is:"+ v4);
+        System.out.println("Enter a float:");
+        float v5 = sc.nextFloat();
+        System.out.println("The float value is:"+v5);
+        System.out.println("Enter a double:");
+        double v6 = sc.nextDouble();
+        System.out.println("The double value is:"+v6);
+        System.out.println("Enter a boolean:");
+        boolean v7 = sc.nextBoolean();
+        System.out.println("The boolean value is: "+v7);
+        System.out.println("Enter a string :");
+        String v8 = sc.next();
+        System.out.println("The string value is:"+v8); // one word from set of words in a string as input
+        String v9 = sc.nextLine();
+        System.out.println("The string value is:"+v9); // whole set of words in a string as input
+
+
+        
+
+
+;
+
+
+
+
+
+
+    }
     
 }
