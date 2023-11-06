0be2681c7e5837734348c142cb33ac9ec287de67
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-06 09:24:58 +0530

    Practical class-1 (assignment questions only) added

--- ClassworkCodes/PracticalClass1.java
+++ ClassworkCodes/PracticalClass1.java
@@ -2,17 +2,8 @@ package ClassworkCodes;
 
 public class PracticalClass1 {
     public static void main(String[] args){
-        //System.out.println(System.out.println("Welcome"));
-        
-        // String + String = String (Concatenation operator)
-        // int + String = String (Concatenation operator)
-        // int + int = int (Arithmetic operator)
+        // Assignment 0-1 questions .
 
-        System.out.println("ABC"+2*3);
-        // "ABC" + 2*3 , * operator is an arithmetic operator , not a concatenation operator 
-        // Output is ABC6 of String type
-        System.out.println(2*3+"ABC"); //6ABC
-        
 
 
     }
---
+++ ClassworkCodes/PracticalClass3.java
@@ -0,0 +1,5 @@
+package ClassworkCodes;
+
+public class PracticalClass3 {
+    
+}
