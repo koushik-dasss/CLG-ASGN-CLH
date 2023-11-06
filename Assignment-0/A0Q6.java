e1649b8a2ca40622884457996ba820bffcee6762
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-06 08:49:16 +0530

    A0Q6 code added

---
+++ Assignment-0/A0Q6.java
@@ -0,0 +1,12 @@
+public class A0Q6 {
+    public static void main(String[] args){
+        String name = "Koushik Das";
+        double CGPA = 10.00;
+        int roll_no = 4117;
+        System.out.println("My name is "+ name+"\nhaving a CGPA of "+ CGPA+"\nand my  roll no. is "+ roll_no);
+
+
+
+    }
+    
+}
