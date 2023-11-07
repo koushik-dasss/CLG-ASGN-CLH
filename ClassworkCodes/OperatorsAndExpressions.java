e412fd760960e16de9d11972aaba446ca9f9b372
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-07 09:35:53 +0530

    Operators and Expressions  explanatio addedn

---
+++ ClassworkCodes/OperatorsAndExpressions.java
@@ -0,0 +1,21 @@
+package ClassworkCodes;
+
+public class OperatorsAndExpressions {
+    public static void main(String[] args){
+        // Types of Operators
+        // ARLAIDCBS
+        // Arithmetic , Logical , Relational , Assignment
+        // Increment and Decrement , Conditional , Bitwise , Special
+
+        // Arithmetic --> constructs mathematical expressions
+
+        // + addition or unary plus
+        // - subtraction or unary minus
+        // * multiplication
+        // / division
+        // % modulo division(remainder)
+
+        
+    }
+    
+}
