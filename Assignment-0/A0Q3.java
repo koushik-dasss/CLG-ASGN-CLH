074f0bfac872dd7f9911da3bfe211cb9c2a09a23
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-08 08:35:40 +0530

    difference between escaping sequence characters \n and \t

--- Assignment-0/A0Q3.java
+++ Assignment-0/A0Q3.java
@@ -4,13 +4,17 @@ public class A0Q3 {
                 System.out.println("My name is Koushik\t Das");
                 System.out.println("My name is Koushik Das\tMy branch is CSIT");
         
-                // \t gives a tab space , but remains in the same line while printing as output in the console 
+                // \t gives a tab space , but the output is in the same line (just added with a tab space)
         
-                // Each \t represents a tab character, and the console or output device will align the text to the next tab stop. 
+                /*  Each \t represents a tab character, and the console or output device 
+                 will align the text to the next tab stop. */
         
                 //This is commonly used to format data in columns when printing information to the console 
         
                 //or when constructing formatted strings.
+
+                // \t = tab space ( single line of code with single line of output but with a tab space)
+                // \n = new line character (single line of code with multiple lines of output)
     }
     
 }
