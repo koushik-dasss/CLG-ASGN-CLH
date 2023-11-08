c5aa0399b9a137cf6d88aea0c66d019f577bed74
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-08 22:34:39 +0530

    A2Q1 added

---
+++ LabAssignment-2/A2Q1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class A2Q1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a degree in Fahrenheit");
+        double fahrenheit = sc.nextDouble();
+        double celsius = (fahrenheit-32) *(5.0/9);
+        System.out.println(fahrenheit+" Fahrenheit is "+ celsius+" Celsius" );
+        
+
+
+
+
+
+    }
+    
+}
