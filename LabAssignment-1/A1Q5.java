f9d596fd27d1567fcf053854de092ed6eb494fce
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-08 09:17:15 +0530

    Optimized the code of displaying the name and message

--- LabAssignment-1/A1Q5.java
+++ LabAssignment-1/A1Q5.java
@@ -1,7 +1,6 @@
 public class A1Q5 {
     public static void main(String[] args){
-        String name = "Koushik";
-        String message = "Good Morning";
+        String name = "Koushik",message = "Good Morning";
         System.out.println("Hey "+ name+", "+ message+"!");
     
     
