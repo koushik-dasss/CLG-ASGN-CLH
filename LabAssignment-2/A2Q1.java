77efeaa2c244351030af6443582978ff17681ec0
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-09 09:16:00 +0530

    A2Q1 added , fahrenheit to celcius non optimised rough code added

--- LabAssignment-2/A2Q1.java
+++ LabAssignment-2/A2Q1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class A2Q1 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a degree in Fahrenheit");
+        System.out.print("Enter a degree in Fahrenheit:");
         double fahrenheit = sc.nextDouble();
         double celsius = (fahrenheit-32) *(5.0/9);
         System.out.println(fahrenheit+" Fahrenheit is "+ celsius+" Celsius" );
