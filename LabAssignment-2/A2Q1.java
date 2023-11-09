681492d638b5e9caf808dfda074ec9cbe6677e35
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-09 20:45:42 +0530

    Added

--- LabAssignment-2/A2Q1.java
+++ LabAssignment-2/A2Q1.java
@@ -3,11 +3,10 @@ public class A2Q1 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a degree in Fahrenheit:");
-        double fahrenheit = sc.nextDouble();
-        double celsius = (fahrenheit-32) *(5.0/9);
+        double fahrenheit = sc.nextDouble(), celsius = (fahrenheit-32) *(5.0/9);
         System.out.println(fahrenheit+" Fahrenheit is "+ celsius+" Celsius" );
         
-
+        sc.close();
 
 
 
--- LabAssignment-2/A2Q2.java
+++ LabAssignment-2/A2Q2.java
@@ -3,11 +3,13 @@ public class A2Q2 {
     public static void main(String[] args){
        Scanner sc = new Scanner(System.in);
        System.out.print("Enter the distance in km =");
-       double d_km = sc.nextDouble(), d_m = d_km*1000,d_feet = d_km*3280.8399,d_inch = d_km*39370.0787,d_cm = d_km*100000;
+       double  d_km = sc.nextDouble(), d_m = d_km*1000,d_feet = d_km*3280.8399,d_inch = d_km*39370.0787,d_cm = d_km*100000;
        System.out.println(d_km+" km is "+d_m+" meters");
        System.out.println(d_km+" km is "+d_feet+" feet");
        System.out.println(d_km+" km is "+d_inch+" inch");
        System.out.println(d_km+" km is "+d_cm+" centimeters");
+
+       sc.close();
 
        
 
