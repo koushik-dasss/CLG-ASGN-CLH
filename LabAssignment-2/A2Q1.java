d88f45ca45a3116feed37d056d68096ee4c5d809
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-09 21:25:33 +0530

    added

--- LabAssignment-2/A2Q1.java
+++ LabAssignment-2/A2Q1.java
@@ -5,6 +5,13 @@ public class A2Q1 {
         System.out.print("Enter a degree in Fahrenheit:");
         double fahrenheit = sc.nextDouble(), celsius = (fahrenheit-32) *(5.0/9);
         System.out.println(fahrenheit+" Fahrenheit is "+ celsius+" Celsius" );
+
+        System.out.print("Enter a degree in Fahrenheit:");
+        int f = sc.nextInt();
+        float c = (f-32)*(float)5/9;
+        System.out.println(f+" Fahrenheit is "+ c+" Celsius" );
+
+
         
         sc.close();
 
