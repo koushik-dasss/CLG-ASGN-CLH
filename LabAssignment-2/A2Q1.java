4a3ac238225575843c9d3b7dd126505cb56280a3
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-09 22:05:31 +0530

    added

--- LabAssignment-2/A2Q1.java
+++ LabAssignment-2/A2Q1.java
@@ -2,14 +2,21 @@ import java.util.Scanner;
 public class A2Q1 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a degree in Fahrenheit:");
-        double fahrenheit = sc.nextDouble(), celsius = (fahrenheit-32) *(5.0/9);
+        /*System.out.print("Enter a degree in Fahrenheit:");
+        double fahrenheit = sc.nextDouble(), celsius = (fahrenheit-32) *(5.0/9); // (float)5/9 == (5.0/9)
         System.out.println(fahrenheit+" Fahrenheit is "+ celsius+" Celsius" );
+         
 
+
+        // According to sample code given in the question
         System.out.print("Enter a degree in Fahrenheit:");
         int f = sc.nextInt();
-        float c = (f-32)*(float)5/9;
-        System.out.println(f+" Fahrenheit is "+ c+" Celsius" );
+        float c = (f-32)*(float)5/9; // (float)5/9 == (5.0/9)
+        System.out.println(f+" Fahrenheit is "+ c+" Celsius" );*/
+
+        
+
+
 
 
         
