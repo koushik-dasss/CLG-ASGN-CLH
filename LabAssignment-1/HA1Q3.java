55f463e1761bddf144740f9f4dd81f5e58082a37
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-10 22:35:26 +0530

    added rough codes

--- LabAssignment-1/HA1Q3.java
+++ LabAssignment-1/HA1Q3.java
@@ -9,13 +9,7 @@ public class HA1Q3 {
         System.out.println(4*a);
 
 
-        float maxFloat = Float.MAX_VALUE;
-        float overflowedResult1 = maxFloat * 2; // Results in positive infinity
-        System.out.println(overflowedResult1);
-
-        float minFloat = Float.MIN_VALUE;
-        float overflowedResult2 = minFloat * 2 ;
-        System.out.println(overflowedResult2);
+        
 
 
     }
---
+++ RoughCodes/RandomCodes.java
@@ -0,0 +1,17 @@
+package RoughCodes;
+import java.util.Scanner;
+
+public class RandomCodes {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        
+        
+        
+        
+        
+        
+        sc.close();
+    }
+    
+}
