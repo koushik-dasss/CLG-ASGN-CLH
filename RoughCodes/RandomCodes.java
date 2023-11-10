b479cf6ba2a9eaa7421a87ec95b16a8fe0600dfb
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-10 22:50:44 +0530

    examples of code added for practice

--- RoughCodes/RandomCodes.java
+++ RoughCodes/RandomCodes.java
@@ -4,6 +4,26 @@ import java.util.Scanner;
 public class RandomCodes {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println(8+9);
+        System.out.println(+(8));
+        System.out.println(-(8));
+        System.out.println(8^2);
+        System.out.println(9-8);
+        System.out.println(8*9);
+        System.out.println(9/8);
+        System.out.println(9%8);
+        System.out.println(14%-3);
+        System.out.println(-14%3);
+        System.out.println(-14%-3);
+        System.out.println(14%3); 
+
+        
+
+
+
+
+
+        
         
         
         
