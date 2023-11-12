dbd2d06c63d6df9aa246c1387eafea29beb1f751
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-12 23:47:34 +0530

    Range values code added

---
+++ ClassworkCodes/MaxValueMinValue.java
@@ -0,0 +1,30 @@
+package ClassworkCodes;
+
+public class MaxValueMinValue {
+    public static void main(String[] args ){
+        int a = Integer.MAX_VALUE,b = Integer.MIN_VALUE;
+        System.out.println("The value of integer data type ranges from : "+ a + " to " + b );
+        short c = Short.MAX_VALUE,d = Short.MIN_VALUE;
+        System.out.println("The value of short data type ranges from : "+ c + " to " + d );
+        byte e = Byte.MAX_VALUE,f = Byte.MIN_VALUE;
+        System.out.println("The value of byte data type ranges from : "+ e + " to " + f );
+        long g = Long.MAX_VALUE,h = Long.MIN_VALUE;
+        System.out.println("The value of long data type ranges from : "+ g + " to " + h );
+        float i = Float.MAX_VALUE,j = Float.MIN_VALUE;
+        System.out.println("The value of float data type ranges from : "+ i + " to " + j );
+        double k = Double.MAX_VALUE,l = Double.MIN_VALUE;
+        System.out.println("The value of double data type ranges from : "+ k + " to " + l );
+    
+
+
+
+
+
+
+
+
+
+
+    }
+    
+}
