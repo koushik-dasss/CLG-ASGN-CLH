f069fe988948314207670b29658ad3fce3430f5c
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-14 21:13:34 +0530

    Underscore can be used between values that are initialiseed with identifiers/variables

---
+++ OutputQuestions/UnderscoreLiterals.java
@@ -0,0 +1,12 @@
+package OutputQuestions;
+
+public class UnderscoreLiterals {
+    public static void main(String[] args){
+        double a = 123.456, b=12_3.4_56,c=1_23.4_5_6;
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+
+    }
+    
+}
