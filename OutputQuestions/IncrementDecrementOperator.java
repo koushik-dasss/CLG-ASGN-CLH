a4e198b118f8da43e21e2afb06474e05f3a0819a
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-14 22:38:57 +0530

    prefix operator and postfix operator difference

---
+++ OutputQuestions/IncrementDecrementOperator.java
@@ -0,0 +1,20 @@
+package OutputQuestions;
+
+public class IncrementDecrementOperator {
+    public static void main(String[] args) {
+        int x = 5 , y;
+        y = ++x;
+        System.out.println(x + "\t" + y);
+        y = x++;
+        System.out.println(x + "\t" + y);
+        y = --x;
+        System.out.println(x + "\t" + y);
+        y = x--;
+        System.out.println(x + "\t" + y);
+
+
+
+        
+    }
+    
+}
