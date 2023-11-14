b6a5fc3f80b4a6ba17b386320b9ace14e17b6344
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-14 21:05:29 +0530

    Assignment operators have higher priority than relational operators

---
+++ OutputQuestions/OperatorPriority.java
@@ -0,0 +1,9 @@
+package OutputQuestions;
+
+public class OperatorPriority {
+    public static void main(String[] args){
+        String S1 = "ITER", S2 ="ITER";
+        System.out.println("S1==S2 " +  S1==S2); 
+    }
+    
+}
