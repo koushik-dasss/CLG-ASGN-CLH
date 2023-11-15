73647d3dbb1cd474ff9af11e45903e1b99142147
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-15 09:27:49 +0530

    Input from user via keyboard code added

--- LabAssignment-1/A1Q6.java
+++ LabAssignment-1/A1Q6.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
 public class A1Q6 {
     public static void main(String[] args){
-        int Account_No = 123456;
+        Scanner sc = new Scanner(System.in);
+        /*int Account_No = 123456;
         String name = "Koushik";
         double balance = 7654.98;
-        System.out.println("My name is "+name+" bearing  account number "+ Account_No+" having  balance  "+ balance);
+        System.out.println("My name is "+name+" bearing  account number "+ Account_No+" having  balance  "+ balance);  */
+
+        // Taking user input via keyboard
+
+        int account_no = sc.nextInt();
+        String name = sc.next();
+        double balance = sc.nextDouble();
+        System.out.println("My name is "+name+" bearing  account number "+ account_no+" having  balance  "+ balance);  
+
+
+
+
+        sc.close();
+        
 
 
     }
