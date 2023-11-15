32cbc3bbdf3e48d53775d58a85261d26bd713006
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-15 20:45:13 +0530

    Taking input from user via keyboard for swapping two variables without using third variable

--- LabAssignment-1/A1Q8.java
+++ LabAssignment-1/A1Q8.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class A1Q8 {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         /*int x=10, y=20;
         x=x+y; //10+20=30
         y=x-y; //30-20=10
@@ -8,11 +10,25 @@ public class A1Q8 {
         System.out.println("Exchanged value of x is : "+ x+"\nExchanged value of y is : "+y);*/
 
 
-        int x=10,y=20;
+        /*int x=10,y=20;
         y=x+y; //10+20=30
         x=y-x; //30-10=20
         y=y-x; //30-20=10
-        System.out.println("Exchanged value of x is:"+x+"\nExchanged value of y is:"+y);
+        System.out.println("Exchanged value of x is:"+x+"\nExchanged value of y is:"+y);*/
+
+        // Taking input from user via keyboard
+        System.out.println("Enter two inetegrs:");
+        int a = sc.nextInt(), b = sc.nextInt();
+        System.out.println("Before swap :  a = "+ a + " b = " + b  );
+        a = a + b ;
+        b = a - b ;
+        a = a - b ;
+        System.out.println("After swap  :  a = "+ a + " b = " + b  );
+
+        
+
+
+        sc.close();
 
     }
     
