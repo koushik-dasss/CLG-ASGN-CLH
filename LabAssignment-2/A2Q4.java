7bc182dcd38c88ae7f781cea4c351e6b0b688ce1
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-15 22:29:57 +0530

    A2Q4 added with few modifications to be done later on

--- LabAssignment-2/A2Q4.java
+++ LabAssignment-2/A2Q4.java
@@ -3,15 +3,8 @@ public class A2Q4 {
     public static void  main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter an integer between 0-1000:");
-        int num = sc.nextInt(), sum, ctr =0;
-        sum = num%10; // 9
-        ctr+=sum; // 0 + 9 = 9
-        sum = num/10; // 74
-        sum = num%10; // 4
-        System.out.println(sum);
-        ctr+=sum; 
-        System.out.println(ctr);
-
+        int num = sc.nextInt(), a = num%10 , b = num/10  , c = b/10 , d = b%10; 
+        System.out.println("The sum of digits is: "+ (a + c + d) );
 
 
 
