d87ea2d67231df66105ec60162efba7ff27d3332
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-15 09:33:43 +0530

    Input from user via keyboard code added for value swapping using third variable

--- LabAssignment-1/A1Q7.java
+++ LabAssignment-1/A1Q7.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class A1Q7 {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         /*int x=10,y=20,z=0;
         z=x; // z = x = 10
         x=y; // x = y = 20
@@ -14,6 +16,29 @@ public class A1Q7 {
         y=x; // y = x = 10
         x=z; // x = z = 20
         System.out.println("Exchanged value of x is:"+x+"\nExchanged value of y is:"+y); */
+        
 
+
+
+        // Taking input from user via keyboard
+        System.out.println("Enter two integers:");
+        int a = sc.nextInt(), b = sc.nextInt(), temp;
+        System.out.println(" Before swap  a = "+ a + " b = " + b );
+        temp = a ;
+        a = b ;
+        b = temp ;
+        System.out.println(" After swap   a = "+  a + " b = " + b );
+
+
+        
+
+
+
+
+
+
+
+
+        sc.close();
     }
 }
