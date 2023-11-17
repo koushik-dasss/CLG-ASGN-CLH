c51f5307b9e3d1a99ead97efa9f0936897b73e6d
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-17 22:28:35 +0530

    Logical operators can only be used on relational statements or boolean value statements not directly on any type of operand

---
+++ OutputQuestionsAssignment/OQ_Q10.java
@@ -0,0 +1,11 @@
+package OutputQuestionsAssignment;
+
+public class OQ_Q10 {
+    public static void main(String[] args) {
+        /*int a = 2;
+        int b = 3;
+         int result = a && b; // can't use logical operator directly on  integer operands.
+        System.out.println(result); */
+    }
+    
+}
