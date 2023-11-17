e192f34c468d29bc2244ab729ddf48fecfb7120a
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-17 13:11:08 +0530

    OQ_Q3 added , increment and decrement operators practice , ++ prefix adds +1 and prints the new value whereas postfix ++ add +1 and prints the old value , same with the decrement operator

---
+++ OutputQuestionsAssignment/OQ_Q3.java
@@ -0,0 +1,14 @@
+package OutputQuestionsAssignment;
+
+public class OQ_Q3 {
+    public static void main(String[] args) {
+        int a=2,b=5,c;
+        a=a*a++ - --a; // a = 2 * 2 - 2 = 4 - 2 = 2
+        c=b++ - b--; // c = 5 - 6 = -1 
+        System.out.println("a="+a+",b="+b+",c="+c); // a = 2 , b = 5 , c = -1 
+        System.out.println(a++ + ++a * a--); // 2 + 4 * 4 = 2 + 16 = 18
+        System.out.println(b=b++ * b--); // b = 5 * 6 = 30
+        System.out.println("a="+a+",b="+b+",c="+c); // a = 3 , b = 5 , c = -1
+    }
+    
+}
