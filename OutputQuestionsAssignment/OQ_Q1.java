4cec0b26e730766eb1b62c6b2db3074a6c9b02a9
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-17 12:04:19 +0530

    Outut question added , Void error == System.out.println(Sytem.out.println)

---
+++ OutputQuestionsAssignment/OQ_Q1.java
@@ -0,0 +1,11 @@
+package OutputQuestionsAssignment;
+
+public class OQ_Q1 {
+    public static void main(String[] args) {
+       // System.out.println(System.out.println("hi"));
+
+       // Void error (Outer System.out.println() statement has nothing to return in the console  , 
+       // nothing to return == void console  
+    }
+    
+}
