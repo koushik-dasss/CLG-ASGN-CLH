192149031094b4710ededdb875568667720008f4
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-17 11:52:23 +0530

    A2Q6 added , modifications required later on

---
+++ LabAssignment-2/A2Q6.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class A2Q6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of seconds: ");
+        double g = 32.174, t = sc.nextDouble(),d = (0.5)*g*t*t;
+        System.out.println("Distance travelled: "+ d);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        sc.close();
+        
+    }
+    
+}
