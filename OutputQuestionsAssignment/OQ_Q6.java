a0acb456fa6835790dc93d98f78f1abde4b68523
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-17 16:36:21 +0530

    explanation to be added soon

---
+++ OutputQuestionsAssignment/OQ_Q6.java
@@ -0,0 +1,12 @@
+package OutputQuestionsAssignment;
+
+public class OQ_Q6 {
+    public static void main(String[] args) {
+        int x = - 4;
+        System.out.println(x>>1);
+        int y = 4;
+        System.out.println(y>>1); 
+       
+    }
+    
+}
