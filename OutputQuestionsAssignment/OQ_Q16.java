7d10f9bbb37421addb702625a83102c5bde07be1
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-18 09:40:27 +0530

    Use of increment and decrement operators , explanation known

---
+++ OutputQuestionsAssignment/OQ_Q16.java
@@ -0,0 +1,12 @@
+package OutputQuestionsAssignment;
+
+public class OQ_Q16 {
+    public static void main(String[] args) {
+        int x=10;
+        System.out.println(x++); // 10
+        System.out.println(++x); // 12
+        System.out.println(x--); // 12
+        System.out.println(--x); // 10
+    }
+    
+}
