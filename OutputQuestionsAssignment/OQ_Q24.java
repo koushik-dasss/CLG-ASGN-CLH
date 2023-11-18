d5620e1ee41fe4a566ac2b79bdcc53ef34f3345a
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-18 22:05:59 +0530

    Assignment shorthand operator practice

---
+++ OutputQuestionsAssignment/OQ_Q24.java
@@ -0,0 +1,17 @@
+package OutputQuestionsAssignment;
+
+public class OQ_Q24 {
+    public static void main(String[] args) {
+        int a=10;
+        a+=3; 
+        System.out.println(a); // 13
+        a-=4;
+        System.out.println(a); // 9
+        a*=2;
+        System.out.println(a); // 18
+        a/=2;
+        System.out.println(a); // 9
+        
+    }
+    
+}
