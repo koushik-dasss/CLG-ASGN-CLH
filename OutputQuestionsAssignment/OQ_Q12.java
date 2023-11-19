cb87962f943a5c64a432b09df55cd9b90264795f
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-19 13:38:53 +0530

    Explanation to be added for Bitwise shift left and shift right operators

---
+++ OutputQuestionsAssignment/OQ_Q12.java
@@ -0,0 +1,9 @@
+package OutputQuestionsAssignment;
+
+public class OQ_Q12 {
+    public static void main(String[] args) {
+        int x=Integer.MAX_VALUE;
+        System.out.println(x>>28);
+    }
+    
+}
