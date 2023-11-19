f020a4251f2cf10d183ea32b6649f47fcfab5889
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-19 09:50:07 +0530

    Explicit type casting of integer to byte data type , where the value ranges outside of byte data type's range , so Typecasted value = ( integer value + 128) % 256 -128 , same apllies for conversion of integer to short and char , if value doesn't lie between ranges of char and short respectively

--- OutputQuestionsAssignment/OQ_Q26.java
+++ OutputQuestionsAssignment/OQ_Q26.java
@@ -2,12 +2,27 @@ package OutputQuestionsAssignment;
 
 public class OQ_Q26 {
     public static void main(String[] args) {
-        int i = 132;
-        short s = 15; 
-        byte b = (byte) i; // result = (value + 128) % 256 - 128 = ( i + 128 ) % 256 - 128 
-        // = ( 132 + 128) % 256 - 128 = ( 260 ) % 256 - 128 = 4 - 128 = -124
+        int i = 132; // integer declared
+        short s = 15;  // short declared 
+        byte b = (byte) i;     //  ( 132 + 128) % 256 - 128 = ( 260 ) % 256 - 128 = 4 - 128 = -124
+ 
+        
+        
+        // Explicit type casting from integer( larger data type) to byte( smaller data type)
+        // The value of integer is out of ranges of byte , so formula is to be used
+        
+        // Formula :- Wrapped value (Integer to byte , out of range for byte) = ( integer value + 128) % 256 - 128
+        
+        // Same formula for Integer to short and char , if the value ranges outside the range of short and char 
+        // after conversion ( Wrapping of value)
+
+
+        
+    
         int x = b + s ;  // -124 (int to byte value) + 15 = -109 
         System.out.println("Value of x is " + x); // -109
+
+        
     }
     
 }
