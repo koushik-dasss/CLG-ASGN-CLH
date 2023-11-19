42289b8e0811f16d50fec75bef96aac04e9eabdc
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-19 09:05:40 +0530

    Coversion of integer value into byte ,if byte value is not in the rangee of it's values, then  byte value = ( integer value + 128 ) % 256 - 128 = Wrapped value of the integer turned to byte , this happens only in implicit type casting

--- OutputQuestionsAssignment/OQ_Q26.java
+++ OutputQuestionsAssignment/OQ_Q26.java
@@ -4,9 +4,10 @@ public class OQ_Q26 {
     public static void main(String[] args) {
         int i = 132;
         short s = 15; 
-        byte b = (byte) i; 
-        int x = b + s ;
-        System.out.println("Value of x is " + x);
+        byte b = (byte) i; // result = (value + 128) % 256 - 128 = ( i + 128 ) % 256 - 128 
+        // = ( 132 + 128) % 256 - 128 = ( 260 ) % 256 - 128 = 4 - 128 = -124
+        int x = b + s ;  // -124 (int to byte value) + 15 = -109 
+        System.out.println("Value of x is " + x); // -109
     }
     
 }
