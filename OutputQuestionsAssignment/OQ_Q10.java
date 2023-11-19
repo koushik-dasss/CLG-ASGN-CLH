734e3e047dd21fa6c1fc7c321761dd7e264fdcbb
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-19 13:23:38 +0530

    Logical AND (&&) operator can't be directly used within two integer operands , while Bitwise (&)AND can be used between two integer operands , which is when both the bits = 1(true) , then only the resultant value is 1(true) , otherwise 0 (false)

--- OutputQuestionsAssignment/OQ_Q10.java
+++ OutputQuestionsAssignment/OQ_Q10.java
@@ -2,10 +2,20 @@ package OutputQuestionsAssignment;
 
 public class OQ_Q10 {
     public static void main(String[] args) {
+        // Use of Logical AND (&&)
+
         /*int a = 2;
         int b = 3;
-         int result = a && b; // can't use logical operator directly on  integer operands.
+         int result = a && b; // can't use logical operator directly on  integer operands. (undefined on integer operands)
         System.out.println(result); */
+       
+        // Use of bitwise AND (&)
+
+        int a = 2;
+        int b = 3;
+        int result = a & b ; // 2 & 3 = 2 
+        // Bitwise AND (&) working -  if both the bits are 1(true) , then only 1(true) will be the output , otheriwse 0(false)
+        System.out.println(result);
     }
     
 }
