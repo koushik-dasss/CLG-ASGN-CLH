6215ccf5eb47c455b79e74e5e6920683d7432059
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-19 12:14:14 +0530

    byte+short+int+long = long , long to byte = (long + 128) % 256 - 128

--- OutputQuestionsAssignment/OQ_Q27.java
+++ OutputQuestionsAssignment/OQ_Q27.java
@@ -2,12 +2,13 @@ package OutputQuestionsAssignment;
 
 public class OQ_Q27 {
     public static void main(String[] args) {
-        long l = 30;
-        int i = 50;
-        short s = 60;
-        byte b = 70;
-        byte sum = (byte)(l + i + s + b);
-        System.out.println("Sum = " + sum); // comment 
+        long l = 30; // long declared
+        int i = 50;  // integer declared
+        short s = 60;// short declared
+        byte b = 70; // byte declared 
+        byte sum = (byte)(l + i + s + b); 
+        // sum = byte (30+50+60+70) = byte (210) = (210 + 128) % 256 - 128 = 338 % 256 - 128 = 82 - 128 = -46
+        System.out.println("Sum = " + sum); // -46
     }
     
 }
