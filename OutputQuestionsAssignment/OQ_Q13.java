685b1ab28edd173f9b0324fc361d2221d3594f32
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-19 16:39:51 +0530

    Use of Bitwise Exclusive OR , Bitwise OR and Bitwise AND operators

---
+++ OutputQuestionsAssignment/OQ_Q13.java
@@ -0,0 +1,9 @@
+package OutputQuestionsAssignment;
+
+public class OQ_Q13 {
+    public static void main(String[] args) {
+        int x=10,y=5;
+        System.out.println(x++^++y|(x=y)&101); // (10 ^ 6)|(6) & 101 ) = 12 | (6) & 101 = 12 | 4 = 12       
+    }
+    
+}
