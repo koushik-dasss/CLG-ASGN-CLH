e14b4cb54259516f9b3fbac465106b11e5549781
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-20 22:08:40 +0530

    Using command line argument for finding quotieent and remainder of two integers , by help of Integer.parseInt() method

--- LabAssignment-2/A2CLAQ1.java
+++ LabAssignment-2/A2CLAQ1.java
@@ -1,9 +1,10 @@
 public class A2CLAQ1 {
     public static void main(String[] args) {
-        int a = Integer.parseInt(args[0]), b = Integer.parseInt(args[1]);
+        int a = Integer.parseInt(args[0]), b = Integer.parseInt(args[1]); 
+        // Use of parseInt for taking input from  the command line argument
         int q = a/b , r = a%b;
-        System.out.println("q = " + q );
-        System.out.println("r = " + r );
+        System.out.println("Quotient is  = " + q );
+        System.out.println("Remainder is = " + r );
 
 
 
