1c91078fc06baa571e6b4abb9119a96a8c8c7e32
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-20 22:04:15 +0530

    Added code of command line arguments

---
+++ LabAssignment-2/A2CLAQ1.java
@@ -0,0 +1,14 @@
+public class A2CLAQ1 {
+    public static void main(String[] args) {
+        int a = Integer.parseInt(args[0]), b = Integer.parseInt(args[1]);
+        int q = a/b , r = a%b;
+        System.out.println("q = " + q );
+        System.out.println("r = " + r );
+
+
+
+
+
+    }
+    
+}
