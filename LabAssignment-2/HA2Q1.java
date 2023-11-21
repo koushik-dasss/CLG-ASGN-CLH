16846ae83f568270ce8c46ad2f100105d5db29c9
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-21 20:53:59 +0530

    Right shift operator - a >> b , result = a/2^b , b bits shifted towards right after a is expressed in binary form of 32 bits

---
+++ LabAssignment-2/HA2Q1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class HA2Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of minutes: ");
+        double t = sc.nextDouble();
+        
+        
+    }
+    
+}
