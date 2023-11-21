544d5c003651fb236fdd1d24a1b616bf41f0acfe
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-21 21:17:39 +0530

    Integer.MAX_VALUE's respresentaion starts from 0 and all other are 1 , 28 bits are shifted right , so current value at 8421 = 0111 = 4+2+1 = 7, Integer.MAX_VALUE >> 28 = 7

--- OutputQuestionsAssignment/OQ_Q12.java
+++ OutputQuestionsAssignment/OQ_Q12.java
@@ -2,8 +2,12 @@ package OutputQuestionsAssignment;
 
 public class OQ_Q12 {
     public static void main(String[] args) {
-        int x=Integer.MAX_VALUE;
-        System.out.println(x>>28);
+        int x=Integer.MAX_VALUE; // Binary representation(32 bits) of Integer.MAX_VALUE = 
+        // 0111 1111 1111 1111 1111 1111 1111  1111
+        // 28 bits to be shifted from right 
+        // 0000 0000 0000 0000 0000 0000 0000 0111
+        // 0111 = 2^2 + 2^1 + 2^0 = 4 + 2 + 1 = 7
+        System.out.println(x>>28); // 0111 = 4 + 2 + 1 = 7
     }
     
 }
