c193b9066624b71056528a55add4fb2c2790a794
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-21 21:02:21 +0530

    One's complement , given = n , (~n) = -(n+1) , one's complement always inverts the given bit , that's 1 to 0 , 0 to 1 , true to false , false to true

--- OutputQuestionsAssignment/OQ_Q11.java
+++ OutputQuestionsAssignment/OQ_Q11.java
@@ -2,8 +2,10 @@ package OutputQuestionsAssignment;
 
 public class OQ_Q11 {
     public static void main(String[] args) {
-        int x=-5;
-        System.out.println(~x);
+        // One's complement 
+        // Formula :- given = n , (~n) = -(n+1)
+        int x=-5; 
+        System.out.println(~x);  // -(-5+1) = -(-4) = 4
     }
     
 }
