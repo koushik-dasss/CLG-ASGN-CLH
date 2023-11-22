ba06c1c181b191b637846d1cad4d9d5d0845b279
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-22 05:56:19 +0530

    Completed with understanding , explanation of right shift will be added later

---
+++ OutputQuestionsAssignment/OQ_Q19.java
@@ -0,0 +1,10 @@
+package OutputQuestionsAssignment;
+
+public class OQ_Q19 {
+    public static void main(String[] args) {
+        System.out.println(10>>2); // 2
+        System.out.println(20>>2); // 5
+        System.out.println(20>>3); // 2
+    }
+    
+}
