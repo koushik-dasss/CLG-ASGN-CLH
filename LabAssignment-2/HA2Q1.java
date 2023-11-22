2ad8d35e1852734d435bf4d59305a7a14ffbab51
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-22 21:07:23 +0530

    Solved HA2Q1 , double and int data type difference only , minutues to days and years , years = minutues/525600 , days = minutues%525600/1440

--- LabAssignment-2/HA2Q1.java
+++ LabAssignment-2/HA2Q1.java
@@ -3,7 +3,28 @@ public class HA2Q1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of minutes: ");
-        double t = sc.nextDouble();
+        double t = sc.nextDouble(); 
+        int years = (int) t/525600 , days = (int) (t%525600)/1440;
+        System.out.println(t + " minutes is approximately " + years + " years and " +  days + " days."); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        sc.close();
         
         
     }
