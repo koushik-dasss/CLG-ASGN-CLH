3230fa33a0af34d71a736b8f415633cfe5b73f41
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-22 05:53:21 +0530

    Solved with understanding , explanation of left shift will be added later

---
+++ OutputQuestionsAssignment/OQ_Q18.java
@@ -0,0 +1,11 @@
+package OutputQuestionsAssignment;
+
+public class OQ_Q18 {
+    public static void main(String[] args) {
+        System.out.println(10<<2); // 40
+        System.out.println(10<<3); // 80
+        System.out.println(20<<2); // 80
+        System.out.println(15<<4); // 240
+    }
+    
+}
