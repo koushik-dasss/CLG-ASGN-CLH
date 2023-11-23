6d751a80ee06aca1421d68e85800357019851d28
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-23 21:28:54 +0530

    Power of b over a , using CLA , Math.pow(a,b) , pow and sqrt method always return double values

---
+++ LabAssignment-2/A2CLAQ2.java
@@ -0,0 +1,7 @@
+public class A2CLAQ2 {
+    public static void main(String[] args) {
+        int a = Integer.parseInt( args[0]) , b = Integer.parseInt( args[1]);
+        System.out.println(Math.pow(a,b));
+    }
+    
+}
