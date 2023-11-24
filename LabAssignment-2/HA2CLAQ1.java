c23b936c38c99a3e539522e7f12dad26d8302d31
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-24 09:16:39 +0530

    SI =( P*R*T)/100

---
+++ LabAssignment-2/HA2CLAQ1.java
@@ -0,0 +1,9 @@
+public class HA2CLAQ1 {
+    public static void main(String[] args) {
+            double p = Double.parseDouble(args[0]), r = Double.parseDouble(args[1]), t = Double.parseDouble(args[2]);
+            double SI = (p*r*t)/100;
+            System.out.println("SI is : " + SI);
+    
+    }
+    
+}
