be4591696a7cd9462dd51553693d3d6e46c3a362
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-24 08:41:09 +0530

    character input by CLA method , conversion of character to ASCII , char ch = args[0].charAt(0); int ASCII = ch ;

---
+++ LabAssignment-2/A2CLAQ6.java
@@ -0,0 +1,8 @@
+public class A2CLAQ6 {
+    public static void main(String[] args) {
+        char ch = args[0].charAt(0);
+        int ASCII = ch ;
+        System.out.println("The ASCII value of the given character is : " + ASCII);
+    }
+    
+}
