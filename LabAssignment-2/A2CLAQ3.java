f45adcb22616f3cf260efb7639094bd536671bdd
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-24 22:39:03 +0530

    Explanation to be added

---
+++ LabAssignment-2/A2CLAQ3.java
@@ -0,0 +1,12 @@
+public class A2CLAQ3 {
+    public static void main(String[] args) {
+        double  a = 1 + (int) (6-1+1)* Math.random(), b = 1 + (int) (6-1+1)* Math.random();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+b); 
+
+
+        
+    }
+    
+}
