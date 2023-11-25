ed330576716893cdd3680b9850f228a02fba0dd7
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-25 19:19:53 +0530

    A3Q1 added , use of if then else conditional statement to check whether the user can cast the vote or not

---
+++ LabAssignment-3/A3Q1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner ;
+public class A3Q1{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the age of the person: ");
+        int age = sc.nextInt();
+        if (age >= 18){
+            System.out.println("You are eligible to cast your vote ");
+        } else{
+            System.out.println("You are not eligible to cast your vote ");
+        }
+        sc.close();
+    }
+}
