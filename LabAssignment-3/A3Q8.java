3900a5732e25a09d1c9d4f638fb0c8944d18e812
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-26 15:46:24 +0530

    A3Q8 added , use of if then , else if the, else then conditional statements to find a point's position in x-y plane , with quadrants being specified , or on any particular axis or it's the origin point

---
+++ LabAssignment-3/A3Q8.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class A3Q8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the x-y co-ordinates of a point :  ");
+        double x = sc.nextDouble() , y = sc.nextDouble();
+        
+
+        if ( x>=1 &&  y>=1) {
+            System.out.println( " ( " +  x  + "," + y + ")" + " is in quadrant I  ");
+
+
+        } else if ( x>=1 && y<=-1){
+            System.out.println( " ( " +  x  + "," + y + ")" + " is in quadrant IV  ");
+
+
+        } else if ( x<=-1 && y>=1){
+            System.out.println( " ( " +  x  + "," + y + ")" + " is in quadrant II ");
+
+
+        } else if ( x<=-1 && y<=-1){
+            System.out.println( " ( " +  x  + "," + y + ")" + " is in quadrant III  ");
+
+        } else if (x==0 && y==0){
+            System.out.println( " ( " +  x  + "," + y + ")" + " is on the origin   ");
+
+        } else if ( x == 0 ){
+            System.out.println( " ( " +  x  + "," + y + ")" + " is on the y-axis ");
+        } else if ( y == 0 ){
+            System.out.println( " ( " +  x  + "," + y + ")" + " is on the x-axis ");
+        }    
+        sc.close();
+    }
+    
+}
