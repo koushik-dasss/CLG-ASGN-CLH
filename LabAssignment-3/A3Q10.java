c25c913b3e79f227cdc2bc11b5a670f96051d1a9
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-26 20:24:50 +0530

    Explanation of switch case to be added later

---
+++ LabAssignment-3/A3Q10.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class A3Q10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter secured marks : ");
+        int marks = sc.nextInt();
+        String grade = " " ;
+        switch (marks/10){
+
+            case 10:
+            case 9 : grade  = "O";
+            break;
+            case 8:  grade  = "A";
+            break;
+            case 7:  grade = "B";
+            break;
+            case 6:  grade = "C";
+            break;
+            case 5:  grade = "D";
+            break;
+            case 4:  grade = "E";
+            break;
+            
+   
+            default: grade = "F";
+            break;
+        }
+           System.out.println(grade);
+           sc.close();
+    }
+}
+        
+
+
+
+
+
+        
+        
+    
