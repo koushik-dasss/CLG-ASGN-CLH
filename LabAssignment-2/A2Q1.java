fd1c1245f4938f4ad39919bcf71c2e7ba0c3261c
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-29 22:27:57 +0530

    Fahrenheit changed to celcius , using double as well as int data type , 5.0/9.0 == 5/9 == 5/9.0 == 5.0/9

--- LabAssignment-2/A2Q1.java
+++ LabAssignment-2/A2Q1.java
@@ -3,15 +3,17 @@ public class A2Q1 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         /*System.out.print("Enter a degree in Fahrenheit:");
-        double fahrenheit = sc.nextDouble(), celsius = (fahrenheit-32) *(5.0/9); // (float)5/9 == (5.0/9)
-        System.out.println(fahrenheit+" Fahrenheit is "+ celsius+" Celsius" );
+        double fahrenheit = sc.nextDouble();
+        double celsius = (fahrenheit-32) *(5.0/9); // (float)5/9 == (5.0/9) == (5/9.0) == (5.0/9.0)
+        System.out.println(fahrenheit+" Fahrenheit is "+ celsius+" Celsius" );*/
          
 
 
         // According to sample code given in the question
-        System.out.print("Enter a degree in Fahrenheit:");
+        
+        /*System.out.print("Enter a degree in Fahrenheit:");
         int f = sc.nextInt();
-        float c = (f-32)*(float)5/9; // (float)5/9 == (5.0/9)
+        float c = (f-32)*(float)5/9; // (float)5/9 == (5.0/9) == (5/9.0) == (5.0/9.0)
         System.out.println(f+" Fahrenheit is "+ c+" Celsius" );*/
 
         
