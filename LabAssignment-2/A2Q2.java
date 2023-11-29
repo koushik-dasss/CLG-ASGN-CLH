f7c94194f7318c244513980093b907bb938dfdb3
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-29 22:55:40 +0530

    Km to m , feet , inch , cm conversion by using different formulas , double data type use

--- LabAssignment-2/A2Q2.java
+++ LabAssignment-2/A2Q2.java
@@ -3,7 +3,11 @@ public class A2Q2 {
     public static void main(String[] args){
        Scanner sc = new Scanner(System.in);
        System.out.print("Enter the distance in km =");
-       double  d_km = sc.nextDouble(), d_m = d_km*1000,d_feet = d_km*3280.8399,d_inch = d_km*39370.0787,d_cm = d_km*100000;
+       double  d_km = sc.nextDouble(); 
+       double d_m = d_km*1000;
+       double d_feet = d_km*3280.8399;
+       double d_inch = d_km*39370.0787;
+       double d_cm = d_km*100000;
        System.out.println(d_km+" km is "+d_m+" meters");
        System.out.println(d_km+" km is "+d_feet+" feet");
        System.out.println(d_km+" km is "+d_inch+" inch");
