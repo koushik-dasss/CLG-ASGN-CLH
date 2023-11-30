b2cfa135d1056542354c8f7599e1b24e78777e37
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-30 22:42:49 +0530

    explanation to be added

---
+++ OutputQuestionsAssignment/OQ_Q35.java
@@ -0,0 +1,20 @@
+package OutputQuestionsAssignment;
+
+public class OQ_Q35 {
+    public static void main(String[] args) {
+        /*int a = 10;
+
+        if(++a==11 || ++a==12)  // if statement braces are missing as there as multiple statements under if here
+        ++a;
+        System.out.println(a);*/    // outside of if statement 
+
+
+        // corrected code :-
+        int a = 10;
+        if (++a==11 || ++a== 12){ // ++a = 10 == 11 == false || ++a = 11 == 12 == false = false || false = false 
+            ++a; // will not be executed , but if executed answer would have been 10 when printed (a = 11)
+        }
+        System.out.println(a); // 12 
+    }
+    
+}
