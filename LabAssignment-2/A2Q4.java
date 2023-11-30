2f26273abf257735a622c168a1b1e80f727dbdcb
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-30 09:33:09 +0530

    Added another method to solve

--- LabAssignment-2/A2Q4.java
+++ LabAssignment-2/A2Q4.java
@@ -2,9 +2,23 @@ import java.util.Scanner;
 public class A2Q4 {
     public static void  main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter an integer between 0-1000:");
-        int num = sc.nextInt(), a = num%10 , b = num/10  , c = b/10 , d = b%10; 
-        System.out.println("The sum of digits is: "+ (a + c + d) );
+        /*System.out.print("Enter an integer between 0-1000:");
+        int num = sc.nextInt();
+        int  a = num%10 , b = num/10  , c = b/10 , d = b%10; 
+        System.out.println("The sum of digits is: "+ (a + c + d) );*/
+
+
+        System.out.print("Enter an integer between 0-1000: ");
+        int num = sc.nextInt() , sum = 0 , r ;
+        r = num%10;
+        sum+=r;
+        num/=10;
+        r = num%10;
+        sum+=r;
+        num/=10;
+        sum+=num;
+        System.out.println("The sum of digits is: "+ sum );
+
 
 
 
