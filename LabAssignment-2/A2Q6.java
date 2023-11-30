de842b1fa894da753a5b7f63ef644f53cce61d04
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-11-30 10:10:11 +0530

    code changed for readability

--- LabAssignment-2/A2Q6.java
+++ LabAssignment-2/A2Q6.java
@@ -3,7 +3,8 @@ public class A2Q6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of seconds: ");
-        double g = 32.174, t = sc.nextDouble(),d = (0.5)*g*t*t;
+        double  t = sc.nextDouble(), g = 32.174 ;
+        double d = (0.5)*g*t*t;
         System.out.println("Distance travelled: "+ d);
 
 
