84901f337bb4101248edd8d9c47321d335926a7f
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-02 09:41:48 +0530

    code addef for some temporary issues in releasing

---
+++ OutputQuestionsAssignment/OQ_Q38.java
@@ -0,0 +1,14 @@
+package OutputQuestionsAssignment;
+
+public class OQ_Q38 {
+    public static void main(String[] args) {
+        int a=12+21*3-9/2; // a = 12 + 63 - 4 = 75 - 4 = 71
+        int b=14-32*4+175/8-3; // b = 14 - 128 + 21 - 3 = -96
+       
+        boolean p=(++a>71&&--b<20); // 
+        System.out.println(p);
+        boolean p1=(b-- == -99 || a-- > 100);
+        System.out.println(p1);
+    }
+    
+}
