a2518a8596a19caa7d80bd32c2f00b0155a0de52
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-03 16:20:22 +0530

    string name and message taken for getting one life of code output

--- LabAssignment-1/A1Q5.java
+++ LabAssignment-1/A1Q5.java
@@ -1,8 +1,8 @@
 public class A1Q5 {
     public static void main(String[] args){
-        String name = "Koushik",message = "Good Morning";
-        System.out.println("Hey "+ name+", "+ message+"!");
-    
+        String name = " Koushik ";
+		String message = " Good Morning ";
+		System.out.println("Hey "+ name +" , " +  message + " ! " );
     
     }
     
