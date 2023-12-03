5715b2fee9ef44da04a325e7aee45b11962ce381
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-03 11:20:50 +0530

    basic code reupdated after revision ,use of forward slash for escaping sequences in JAVA , comments of explanation added , \  for double quote escaping sequence inside a string , \' for single quote inside string , \ , everything to be done with \ forward slash only

--- LabAssignment-1/A1Q1.java
+++ LabAssignment-1/A1Q1.java
@@ -1,10 +1,18 @@
 public class A1Q1 {
-    public static void main(String[] args){
-        System.out.println("Hello \"ITERIAN\" ");
-        System.out.println("Welcome to Siksha \'O\' Anusandhan Family ");
-        System.out.println("Wecome to \"Introduction to Computer Laboratory\"");
-        System.out.println("Java is fun for All !!");
-    
+
+	public static void main(String[] args) {
+		System.out.println("Hello \"ITERIAN\" ");
+		System.out.println("Welcome to Siksha \'0\' Anusandhan Family.");
+		System.out.println("Welcome to \"Introduction to Computer Laboratory\"");
+		System.out.println("Java is fun for All !!");
+        
+		
+// \" - used as an escaping sequence to insert double  quotes string inside another string
+// \' - used to insert a single quote string inside another string
+// Use of forward slash is to be kept in mind " \" \" " , " \' \' "
+
+
+		        
+
     }
 }
-
