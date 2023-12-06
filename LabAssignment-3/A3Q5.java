d42d33fcb9f1a7f57da44fc4a746ce541502ef4f
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-06 21:25:24 +0530

    Changed the code due to space complexity issue , deleted two unnecessary variables , created a string for showing boolean true and false as if and else code block are absolutely defined

--- LabAssignment-3/A3Q5.java
+++ LabAssignment-3/A3Q5.java
@@ -5,12 +5,10 @@ public class A3Q5 {
         System.out.print("Enter the year : ");
         int year = sc.nextInt();
         if ((year % 4 == 0 && year % 100 != 0 ) || (year % 400 == 0)){
-            boolean b = true ;
-            System.out.println(year + " is a leap year : " + b);
+            System.out.println(year + " is a leap year :  true ");
 
         } else {
-            boolean b = false ;
-            System.out.println(year + " is a leap year : " + b );
+            System.out.println(year + " is a leap year :  false" );
         }
         
         
