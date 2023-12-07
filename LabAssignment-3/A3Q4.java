e39380d5bf75f5330bf876b130fc65c210919122
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-07 09:44:39 +0530

    A3Q4 added , computer guesses using Math.random() method

--- LabAssignment-3/A3Q4.java
+++ LabAssignment-3/A3Q4.java
@@ -1,21 +1,25 @@
 import java.util.Scanner;
-public class A3Q4 {
+public class RoughCodes {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter user number (1-9, both  inclusive)");
+        System.out.println(Math.random());
+        System.out.print("Enter user number :");
         int UserValue = sc.nextInt();
-        System.out.println("Computer guesses(1-9, both inclusive)");
-        int ComputerValue = sc.nextInt();
-        if ( ComputerValue == UserValue ){
+        System.out.print("Computer guesses :");
+        int ComputerValue = (1 + (int)((10)*Math.random()-1));
+        System.out.println(ComputerValue);
+        if (ComputerValue == UserValue){
             System.out.println("You got it right ");
-        } else if ((ComputerValue == UserValue + 1 )||(ComputerValue == UserValue -1)) {
+
+        }else if ((ComputerValue == UserValue + 1 || ComputerValue == UserValue - 1 )){
             System.out.println("Almost got it ");
-        
-        } else {
+        }else {
             System.out.println("You got it wrong ");
         }
-        sc.close();
-        
+
+
+
+
     }
-    
+
 }
