ccacf879150cecd3fce745e898b302f7d9f32ecb
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-07 21:37:13 +0530

    else code removed as it was unnecessary

--- LabAssignment-3/A3Q1.java
+++ LabAssignment-3/A3Q1.java
@@ -6,8 +6,7 @@ public class A3Q1{
         int age = sc.nextInt();
         if (age >= 18){
             System.out.println("You are eligible to cast your vote ");
-        } else{
-            System.out.println("You are not eligible to cast your vote ");
+        
         }
         sc.close();
     }
