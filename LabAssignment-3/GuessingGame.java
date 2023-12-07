25166b8cd4ef8aa22df5e907a15193f2b8c91d35
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-07 21:35:48 +0530

    Made the guessing game of computer with self with while looping for making the game of certain rounds

---
+++ LabAssignment-3/GuessingGame.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class GuessingGame {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rounds you want to play : ");
+        int round = sc.nextInt(), ctr = 0 ;
+        while (ctr<=round){
+            System.out.print("Enter user number :");
+        int UserValue = sc.nextInt();
+        System.out.print("Computer guesses :");
+        int ComputerValue = (1 + (int)((10)*Math.random()-1));
+        System.out.println(ComputerValue);
+        if (ComputerValue == UserValue){
+            System.out.println("You got it right ");
+
+        }else if ((ComputerValue == UserValue + 1 || ComputerValue == UserValue - 1 )){
+            System.out.println("Almost got it ");
+        }else {
+            System.out.println("You got it wrong ");
+        }
+        ctr++;
+
+
+
+
+
+        }
+        
+    }
+
+}
+
+
+
+    
+    
+
