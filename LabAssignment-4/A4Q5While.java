31bfddaccf14e302fc7ed32492bd6ae558d82aa8
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-10 18:56:00 +0530

    A4Q5 added , using while loop , to find out the number is perfect or not , iteration of while loop should be done inside while loop only not with if condition , if code block ended , the while loop closed after iteration of i

---
+++ LabAssignment-4/A4Q5While.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class A4Q5While {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(Sytem.in);
+        System.out.println("Enter a number : ");
+        int num = sc.nextInt() , sum = 0, i = 1 ;
+        while (i<=(num/2)){
+            if (num%i==0){
+                sum+=i;
+
+            }
+            i++;
+        } if (sum == num){
+            System.out.println(num+" is a perfect number .");
+        } else{
+            System.out.println(num+" is not a perfect number .");
+        }
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        sc.close();
+
+    }
+    
+}
