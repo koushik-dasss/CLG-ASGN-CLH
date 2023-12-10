39222ebec64bf96b58b7c62d2aebe326d5cdecdf
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-10 21:27:44 +0530

    A4Q6 added , finding the base^power using for loop , for (int i = 1 ; i<=power ; i++) { result*=base ; } sopln(resukt)

---
+++ LabAssignment-4/A4Q6For.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class A4Q6For {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the base  : ");
+        int base  = sc.nextInt() ;
+        System.out.println("Enter the power :");
+        int power = sc.nextInt();
+        int  result = 1  ;
+        for (int i = 1 ; i<= power ; i++){
+            result*=base;
+            
+
+        }
+        System.out.println(result);
+
+
+
+        sc.close();
+    }
+
+}
