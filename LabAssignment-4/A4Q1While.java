38c10fe4915d38a936fab885fb855e0fae010c02
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-11 08:43:44 +0530

    A4Q1 added , finding the sum of the numbers between first and second number using third number incrementation , using whil e loop , logic :- while FirstNumber <= SecondNumber , FirstNumber+=IncrementValue , but with sum+=FirstNumber

---
+++ LabAssignment-4/A4Q1While.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class A4Q1While {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two integers : ");
+        int FirstNumber = sc.nextInt()  , SecondNumber = sc.nextInt() ;
+        System.out.println("Enter the increment value : ");
+        int IncrementThirdNumber = sc.nextInt(), sum = 0;
+        while (FirstNumber<= SecondNumber){
+            System.out.print(FirstNumber + " " );
+            sum+=FirstNumber;
+            FirstNumber+=IncrementThirdNumber;
+        }
+
+        System.out.println("\nThe sum of number displayed is " +  sum );
+
+
+
+
+
+
+        sc.close();
+    }
+    
+}
