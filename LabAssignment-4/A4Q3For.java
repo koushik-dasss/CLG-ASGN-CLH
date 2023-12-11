b767f06028fce35c77855ddc185976d2371d7024
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-11 22:23:10 +0530

    A4Q3 added , using for  loop , to find out the average of random numbers that are inputted by using Math.Random()

---
+++ LabAssignment-4/A4Q3For.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class A4Q3For {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for ( int ctr = 1  ; ctr<=n ;ctr++  ){
+            int  Random_Numbers = (1 + (int) ((n + 1) * Math.random() - 1) );
+            System.out.print(Random_Numbers + " " );
+            sum+=Random_Numbers;
+
+
+        }
+        System.out.print("\nAverage of "  + n + " random numbers are : ");
+        double  average = sum/ n;
+        System.out.println(average);
+
+
+
+
+        sc.close();
+    }
+    
+}
