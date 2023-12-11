4b1411699185678f7f45a55cf3fcecd5b6f43d2d
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-11 08:35:59 +0530

    A4Q1 added , first number is added till second number , with an incrementation of a third number , and then the elements a s well as the sum is displayed in the final print statement

---
+++ LabAssignment-4/A4Q1For.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class A4Q1For {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter two integers : ");
+        int FirstNumber = sc.nextInt()  , SecondNumber = sc.nextInt() ;
+        System.out.println("Enter the increment value : ");
+        int IncrementThirdNumber = sc.nextInt(), sum = 0;
+        for (int i = FirstNumber ; i<= SecondNumber ; i+=IncrementThirdNumber){
+            System.out.print(i + "  ");
+            sum+=i;
+
+        }
+
+        System.out.println("\nThe sum of number displayed is " +  sum );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        sc.close();
+    }
+    
+}
