acf28398309f9f730770b98e278172c6d5ff4ef4
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-12 20:47:41 +0530

    Using for loop but with only one variable , optimized code

---
+++ PatternTypeQuestions/0_to_9_9_to_0_For2.java
@@ -0,0 +1,27 @@
+/* Pattern =>    0   9
+                 1   8
+                 2   7
+                 3   6
+                 4   5
+                 5   4
+                 6   3
+                 7   2
+                 8   1
+                 9   0
+
+ */
+
+
+
+import java.util.Scanner;
+public class 0_to_9_9_to_0_For2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0 ; i<=9 ;  i++){
+            System.out.println(i+"\t"+(9-i));
+
+        }
+
+    }
+}
+
