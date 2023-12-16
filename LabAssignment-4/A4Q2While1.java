64af3ed0545364816deaf958d16497b39c516bc6
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-16 09:18:52 +0530

    Renamed file , used while loop to find the sum of the digits and then checked whether the sum is dib by 9 , so as to find whether the number is divisible by 9 , took one extra variable for dispalying according to sample code

--- LabAssignment-4/A4Q2While1.java
+++ LabAssignment-4/A4Q2While1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class A4Q2While {
+public class A4Q2While1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number : ");
