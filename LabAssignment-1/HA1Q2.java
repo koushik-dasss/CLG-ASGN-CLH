8f66993058f308782f0de9eb731b667ceb851940
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-19 12:56:22 +0530

    Explanation of character involved in arithmetic expression added , char + char = int , int coversion from char data type

--- LabAssignment-1/HA1Q2.java
+++ LabAssignment-1/HA1Q2.java
@@ -1,9 +1,15 @@
 public class HA1Q2 {
     public static void main(String[] args){
-        System.out.println('b');
-        System.out.println('b'+'c');
-        System.out.println((char)('a'+4));
+    
+        System.out.println('b'); // answer:- b (only the char value will be printed)
+        System.out.println('b'+'c'); // when two char values are involved in an arithmetic expression ,
+        //  they are treated as an int data type , so 'b' = 97 , 'c' = 98 
+        // 'b' + 'c' = 97 + 98 = 196
+        System.out.println((char)('a'+4)); // 'a' will be treated as a character 
+        // a = 96
+        // (char) ( 97 + 4 ) = (char) (101) = 'e' 
 
+        
 
     }
     
---
+++ LabAssignment-4/A4Q2For2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class A4Q2For2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt(), sum = 0 , r ;
+        System.out.print(n);
+        for ( ; n!=0 ; n/=10) {
+            r = n % 10;
+            sum += r;
+
+
+        }if (sum%9 == 0){
+            System.out.println(" is divisible by 9 ");
+        } else{
+            System.out.println(" is not divisible by 9");
+
+        }
+
+        
+        
+        sc.close();
+    }
+    
+}
