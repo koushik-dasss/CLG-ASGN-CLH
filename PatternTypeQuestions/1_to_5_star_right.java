686dd8e70f5434c4e01a894f887c56d00dbfc624
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-23 12:47:36 +0530

    1 star to 5 star , from right side , pattern , using nested for loop

---
+++ PatternTypeQuestions/1_to_5_star_right.java
@@ -0,0 +1,18 @@
+
+public class 1_to_5_star_right {
+    public static void main(Strinh[] args){
+        for (int i = 1; i <=5 ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                System.out.print("*" + " ");
+
+            }
+            System.out.println();
+
+        }
+
+        sc.close();
+        
+    }
+    
+    
+}
