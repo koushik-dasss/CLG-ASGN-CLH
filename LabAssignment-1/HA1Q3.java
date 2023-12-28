055c124aa4d5e4c6d5401d6e5a8e05d4a01840fa
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-28 22:52:56 +0530

    Logic-: Integer overflow , concept used is overflowing of int data type , out of ranges

--- LabAssignment-1/HA1Q3.java
+++ LabAssignment-1/HA1Q3.java
@@ -1,12 +1,18 @@
 public class HA1Q3 {
     public static void main(String[] args){
-        int a = Integer.MAX_VALUE; // 2,147,483,647
-        System.out.println(a);
-        System.out.println(a+1);
-        System.out.println(2-a);
-        System.out.println(-2-a);
-        System.out.println(2*a);
-        System.out.println(4*a);
+
+        //  Max. and Min. Values of Integer class in JAVA
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        
+        
+        int a = Integer.MAX_VALUE; // 2147483647
+        System.out.println(a); // 2147483647
+        System.out.println(a+1); // 2147483647 + 1 = -(Integer.MIN_VALUE) = -(2147483648) = -2147483648
+        System.out.println(2-a); // -(21474483647-2) = -21474483647
+        System.out.println(-2-a); // -(21474483647+2) = 21474483649
+        System.out.println(2*a); // -2 (formula = even no.* Integer.MAX_VALUE = negative of that even no.)
+        System.out.println(4*a); // -4 (formula = even no.* Integer.MAX_VALUE = negative of that even no.)
 
 
         
