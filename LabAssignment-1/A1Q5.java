5045f9ffb6ab3b2acb1ed6becef4c56f7b7928e8
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-28 14:27:14 +0530

    Logic:- Taking input , two strings , using them to print the output

--- LabAssignment-1/A1Q5.java
+++ LabAssignment-1/A1Q5.java
@@ -1,8 +1,7 @@
 public class A1Q5 {
     public static void main(String[] args){
-        String name = " Koushik ";
-		String message = " Good Morning ";
-		System.out.println("Hey "+ name +" , " +  message + " ! " );
+        String name = "Koushik" , message = "Good Morning";
+        System.out.println("Hey "+ name + " , "+ message +" ! ");
     
     }
     
