8634adb17dd87e10babe3f6b7f80544e7a45cb64
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-28 14:24:50 +0530

    Logic:- Use of \n , escaping sequence's newline character , for printing the code in two lines of output , while the code is in single line of input

--- LabAssignment-1/A1Q4.java
+++ LabAssignment-1/A1Q4.java
@@ -1,32 +1,45 @@
-// Experimental Code ( Not based on exact A1Q4.java)
+
 public class A1Q4 {
 
 	public static void main(String[] args) {
-		System.out.println("Koushik\nDas"); // Koushik
-		                                    // Das
-		System.out.println("Koushik");      // Koushik
-		System.out.println("Das");          // Das
-		String S1 = "Koushik\nDas";         
-		System.out.println(S1);             // Koushik
-		                                    // Das
-		String S2 = "Koushik";
-		String S3 = "Das";
-		String S4 = S2 + S3; // "Koushik\nDas" (\n is considered here as a string)
-		System.out.println(S1.equals(S4)); // false 
-		System.out.println(S4); // "Koushik\nDas" 
-		
-		String S5 = "\n";
-		String S6 = S2 + S5 + S3; 
-		System.out.println(S6); // Koushik\nDas
-		System.out.println(S6.equals(S1)); //true 
+		System.out.println("Koushik\nDas");
+
+
 		
 		
 		/*  New line character ( escaping sequence) \n  (forward slash n)
 		  shows the output  in a new line being in a same line of code 
 		  It just takes the code into the next line immediately after it's use, 
-		  basically formation of a new line of code.
+		  basically formation of a new line of code in output , but the input code 
+		  is done in the same one line of code .
 		  
 		  */
+
+
+
+		// Experimental Code ( Not based on exact A1Q4.java)
+		System.out.println("Koushik");
+        System.out.println("Das");
+
+        System.out.println("Koushik\nDas");
+
+        String S1 = "Koushik" , S2 = "\n" , S3 ="Das";
+        System.out.println(S1+S2+S3);
+
+        String S4 = "Koushik\n" , S5 = "Das";
+        System.out.println(S4+S5);
+
+        String S6 ="Koushik" , S7 = "\nDas";
+        System.out.println(S6+S7);
+
+       
+
+
+ 
+		  
+
+
+
 	}
 
 }
