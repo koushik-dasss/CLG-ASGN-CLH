13789596b9fa3a7b6637ea10a3aea8e08c45e7ad
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-28 22:57:21 +0530

    Logic:- division of floating point data types with integer data type and explicit type casting

--- LabAssignment-1/HA1Q4.java
+++ LabAssignment-1/HA1Q4.java
@@ -1,11 +1,11 @@
 public class HA1Q4 {
     public static void main(String[] args){
         double a = 3.14159;
-        System.out.println(a);
-        System.out.println(a+1);
-        System.out.println(8/(int) a);
-        System.out.println(8/a);
-        System.out.println((int) (8/a));
+        System.out.println(a); // 3.14159
+        System.out.println(a+1); // 3.14159 + 1 = 4.14159
+        System.out.println(8/(int) a); // 8/(int) (3.14159) = 8/3 = 2 
+        System.out.println(8/a); // 8/3.14159 =  2.5464812403910124
+        System.out.println((int) (8/a)); // (int)(8/3.14159) = 2
 
     }
     
