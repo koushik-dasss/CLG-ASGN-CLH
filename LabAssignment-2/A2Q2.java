11fb05b071d5dc955adcf270d55349dc5d00a36b
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-30 17:27:55 +0530

    Logic:- Conversion of distance in km to m,cm,inch,feet , use of double and int data types properly

--- LabAssignment-2/A2Q2.java
+++ LabAssignment-2/A2Q2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class A2Q2 {
     public static void main(String[] args){
        Scanner sc = new Scanner(System.in);
-       System.out.print("Enter the distance in km =");
+       /*System.out.print("Enter the distance in km =");
        double  d_km = sc.nextDouble(); 
        double d_m = d_km*1000;
        double d_feet = d_km*3280.8399;
@@ -11,7 +11,20 @@ public class A2Q2 {
        System.out.println(d_km+" km is "+d_m+" meters");
        System.out.println(d_km+" km is "+d_feet+" feet");
        System.out.println(d_km+" km is "+d_inch+" inch");
-       System.out.println(d_km+" km is "+d_cm+" centimeters");
+       System.out.println(d_km+" km is "+d_cm+" centimeters"); */
+       
+
+       // Using int and double data types  according to their needs . 
+       
+       int d_km = sc.nextInt();
+       int d_m = d_km*1000;
+       double d_feet = d_km*3280.8399;
+       double d_inch = d_km*39370.0787;
+       int d_cm = d_km*100000;
+       System.out.println(d_km + " is "+ d_m +" meters ");
+       System.out.println(d_km + " is "+ d_feet + " feet ");
+       System.out.println(d_km + " is "+ d_inch + " inch ");
+       System.out.println(d_km + " is "+ d_cm + " centimeters "); 
 
        sc.close();
 
