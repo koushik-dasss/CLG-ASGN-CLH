fb3225ed1435ae1d3a48ad8232936a4aff385518
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-30 20:30:12 +0530

    Logic:- A three digit number is there . To extract it's unit digit , we have to find out the remainder of it when divided by 10  and when divided by 10 , it will get updated and it will give the new remainder which is the digit of the number only . Use of % for extracting the digits and use of / for updating the number . Can be done using arithmetic way , or using the logic of loops . r = n%10 , sum+=r , n/=10

--- LabAssignment-2/A2Q4.java
+++ LabAssignment-2/A2Q4.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 public class A2Q4 {
     public static void  main(String[] args){
         Scanner sc = new Scanner(System.in);
+        // Non-optimized logic:- 
+
+        // (Arithmetic way)
         /*System.out.print("Enter an integer between 0-1000:");
         int num = sc.nextInt();
         int  a = num%10 , b = num/10  , c = b/10 , d = b%10; 
         System.out.println("The sum of digits is: "+ (a + c + d) );*/
 
-
-        System.out.print("Enter an integer between 0-1000: ");
+        // Without using any loop :- (logic of loop used in normal code without loop)
+        /*System.out.print("Enter an integer between 0-1000: ");
         int num = sc.nextInt() , sum = 0 , r ;
         r = num%10;
         sum+=r;
@@ -17,7 +20,31 @@ public class A2Q4 {
         sum+=r;
         num/=10;
         sum+=num;
-        System.out.println("The sum of digits is: "+ sum );
+        System.out.println("The sum of digits is: "+ sum );*/
+
+        // Using loops:-
+
+        // While loop :-
+        /*System.out.print("Enter a number between 0 and 1000: ");
+        int n = sc.nextInt() , sum = 0 , r ;
+        while (n!=0){
+            r = n%10;
+            sum+=r;
+            n/=10;
+        }
+        System.out.println("The sum of the digits is : "+ sum);*/
+
+        // For loop :-
+
+        System.out.print("Enter a number between 0 and 1000: ");
+        int n = sc.nextInt() , sum = 0 , r ;
+        for ( ; n!=0; ){
+           r = n%10;
+           sum+=r;
+           n/=10;
+        }
+        System.out.println("The sum of the digits is : "+ sum);
+
 
 
 
