12c5fd8ddd89bedeeaa5010eaeddbee53270380c
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-30 16:49:19 +0530

    Logic:- Taking inputs of four integers from keyboard input , then swapping it using temp variable and without using temp variable , without using temp variable , it's to be done  USING REVERSE OF VARIABLES ORDER  whereas USING NORMAL ORDER ACCORDING TO THE QUESTIOn when used temp variable , take values and experiment to get the answer

--- LabAssignment-1/HA1Q5.java
+++ LabAssignment-1/HA1Q5.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class HA1Q5 {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         // non-optimized code (extra one variable is declared , can be done using 4 variables's arithmetic operations )
         /*int W=10, G=20,K=30,A=40, temp= 0; // USING EXTRA VARIABLE 
         temp=W; // temp = 10
@@ -16,7 +18,7 @@ public class HA1Q5 {
         // optimised code with 4 variables (arithmetic operations)
         // WITHOUT USING EXTRA VARIABLE
 
-        int W=10, G=20,K=30,A=40;
+        /* int W=10, G=20,K=30,A=40;
         // the value of W will move to A, A to K, K to G and finally G to W
 
         W=A+G+K+W; 
@@ -29,14 +31,49 @@ public class HA1Q5 {
         System.out.println(W);
         System.out.println(G);
         System.out.println(K);
-        System.out.println(A);
+        System.out.println(A); */
 
-
+        // Taking input from user(keyboard input)
         
+        // Non-Optimized code(USING TEMP. VARIABLE)
+       /*  System.out.println("Enter four integers: ");
+        int W = sc.nextInt() ,G = sc.nextInt() ,K= sc.nextInt() , A = sc.nextInt();
+        int temp = W;
+        System.out.println("Before swapping : ");
+        System.out.println("W = "+W);
+        System.out.println("G = "+G);
+        System.out.println("K = "+K);
+        System.out.println("A = "+A);
+        W = G ;
+        G = K;
+        K = A;
+        A = temp;
+        System.out.println("After swapping : ");
+        System.out.println("W = "+W);
+        System.out.println("G = "+G);
+        System.out.println("K = "+K);
+        System.out.println("A = "+A); */
         
 
+        System.out.println("Enter four integers: ");
+        int W = sc.nextInt() ,G = sc.nextInt() ,K= sc.nextInt() , A = sc.nextInt();
+        System.out.println("Before swapping : ");
+        System.out.println("W = "+W);
+        System.out.println("G = "+G);
+        System.out.println("K = "+K);
+        System.out.println("A = "+A);
 
-
+        W = W + G + K + A ;
+        A = W - ( G+ K + A);
+        K = W - (G + K + A);
+        G = W - ( G+ K + A);
+        W = W - ( G+ K + A);
+        System.out.println("After swapping : ");
+        System.out.println("W = "+W);
+        System.out.println("G = "+G);
+        System.out.println("K = "+K);
+        System.out.println("A = "+A);
+        sc.close();
     }
     
 }
