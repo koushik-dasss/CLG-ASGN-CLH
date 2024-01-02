62a96ad994189fd02a1da6bad193566fe3d06bd2
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-01-02 12:02:03 +0530

    Logic:- taking input of radius in double form and calculating the surface area and volume of the hemisphere

--- LabAssignment-2/A2Q5.java
+++ LabAssignment-2/A2Q5.java
@@ -3,11 +3,11 @@ public class A2Q5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the radius of the hemisphere: ");
-        double radius = sc.nextDouble();
-        double area =  3*Math.PI*radius*radius ;
-        double volume = (2.0/3)*Math.PI*radius*radius*radius;
-        System.out.println("The surface area of the hemisphere is :" + area );
-        System.out.println("The volume of the hemisphere is :"  + volume );
+        double  r = sc.nextDouble();
+        double SA = 3*Math.PI*r*r;
+        double volume = (2.0/3)*Math.PI*r*r;
+        System.out.println("The surface area of the hemisphere is : " + SA);
+        System.out.println("The volume area of the hemisphere is : " +  volume );
 
 
 
