4dda0dbf3ab4418aff396282425c5c27d0ab4158
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-01-02 12:19:32 +0530

    Logic:- using tab spaces , to print the first line , then the integers , then their power raised using Math.pow method , type casted to int as Math module's method always results in double value , incrementing the integers in the same line of code using postfix incrementation operator

--- LabAssignment-2/A2Q7.java
+++ LabAssignment-2/A2Q7.java
@@ -1,12 +1,29 @@
 public class A2Q7 {
     public static void main(String[] args) {
+
+        //Non-Optimized method(Incrementing the values with separate lines of code)
+
+       /*  System.out.println("a\tb\tpow(a,b)");
+        int a = 1 , b = 2 ;
+        System.out.println(a+"\t"+b+"\t"+(int)(Math.pow(a,b)));
+        a+=1;
+        b+=1;
+        System.out.println(a+"\t"+b+"\t"+(int)(Math.pow(a,b)));
+        a+=1;
+        b+=1;System.out.println(a+"\t"+b+"\t"+(int)(Math.pow(a,b)));
+        a+=1;
+        b+=1;System.out.println(a+"\t"+b+"\t"+(int)(Math.pow(a,b)));
+        a+=1;
+        b+=1;System.out.println(a+"\t"+b+"\t"+(int)(Math.pow(a,b)));
+        a+=1;
+        b+=1;    */
+        // Optimized method(Incrementing the values of a and b , by using postfix incrementation operator)
         System.out.println("a"+"\tb"+"\tpow(a,b)");
         int a = 1 , b = 2 ;
         System.out.println(a+"\t"+b+"\t"+(int)Math.pow(a++,b++));
         System.out.println(a+"\t"+b+"\t"+(int)Math.pow(a++,b++));
         System.out.println(a+"\t"+b+"\t"+(int)Math.pow(a++,b++));
         System.out.println(a+"\t"+b+"\t"+(int)Math.pow(a++,b++));
-
-    }
+   }
     
 }
