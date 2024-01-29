b8df3ef7ef5afa72c44aec64b15c47b50fe2d7b8
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-01-29 13:10:56 +0530

    No parameter  and no return type method

---
+++ Methods Codes/NPNR.java
@@ -0,0 +1,9 @@
+public class NPNR {
+    public static void message(){ // message() => Method header/prototype
+        System.out.println("Read ICP, exam is coming soon "); // Method body 
+    }
+    public static void main(String[] args) {
+       message(); // Method call 
+    }
+    
+}
