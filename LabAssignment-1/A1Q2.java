e7a8fc8cdfa13d8305d52c4f74f54a9904f55498
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-02-07 23:52:19 +0530

    A1Q2 added

--- LabAssignment-1/A1Q2.java
+++ LabAssignment-1/A1Q2.java
@@ -2,7 +2,7 @@ public class A1Q2 {
 
 	public static void main(String[] args) {
 		System.out.println("*******     *******");
-		System.out.println("      *     *     *");
+		System.out.println("      *    *     *");
 		System.out.println("      *******     *******");
 
 
