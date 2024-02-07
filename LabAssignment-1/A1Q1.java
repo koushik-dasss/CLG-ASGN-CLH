ca057fa84314b453bf4cd14cacc28d03264d1b8d
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-02-07 23:51:07 +0530

    A1Q1 added

--- LabAssignment-1/A1Q1.java
+++ LabAssignment-1/A1Q1.java
@@ -1,15 +1,13 @@
 public class A1Q1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello \"ITERIAN\" ");
-		System.out.println("Welcome to Siksha \'0\' Anusandhan Family.");
-		System.out.println("Welcome to \"Introduction to Computer Laboratory\"");
+		System.out.println("Hello \"ITERIAN\" "); //         \" \" = " "
+		System.out.println("Welcome to Siksha \'0\' Anusandhan Family."); //     \' \' =  ' '
+		System.out.println("Welcome to \"Introduction to Computer Laboratory\""); //     \" \" = " "
 		System.out.println("Java is fun for All !!");
         
 		
-// \" - used as an escaping sequence to insert double  quotes string inside another string
-// \' - used to insert a single quote string inside another string
-// Use of forward slash is to be kept in mind " \" \" " , " \' \' "
+
 
 
 
