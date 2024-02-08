2b2173c3139acd83d9f7ec2a8fc8cfe57cab1426
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-02-08 21:06:33 +0530

    A5Q6 b added

---
+++ LabAssignment-5/A5Q6_b.java
@@ -0,0 +1,11 @@
+public class A5Q6_b {
+    public static void main(String[] args) {
+        for(int i = 1 ; i<= 5 ; i++){
+            for(int j = 1 ; j<= i ; j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
